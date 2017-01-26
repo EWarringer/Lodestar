f568336bce956490e622292feaafce6d771978a1
Erik Warringer <Erik@Eriks-MacBook-Pro.local>
2017-01-26 10:58:09 -0500

    user_sign_up acceptance tests

--- spec/features/users/user_signs_up_spec.rb
+++ spec/features/users/user_signs_up_spec.rb
@@ -7,38 +7,42 @@ feature 'user registers', %Q{
 } do
 
 # Acceptance Criteria:
-# * I must specify a valid email address,
-#   password, and password confirmation
-# * If I don't specify the required information, I am presented with
-#   an error message
+# * I must specify all the required information
+# * If I don't I am presented with an error message
+# * I am notified if I confirm the wrong password.
 
   scenario 'provide valid registration information' do
     visit root_path
     click_link "Sign Up"
-    within('#new-user') do
-      fill_in 'First Name', with: 'Jeff'
-      fill_in 'Last Name', with: 'Sampson'
-      fill_in 'Username', with: 'Swansonite11'
-      fill_in 'Email', with: 'jeffsampson@example.com'
-      fill_in 'user_password', with: 'password'
-      fill_in 'Password Confirmation', with: 'password'
+    fill_in 'First Name', with: 'Jeff'
+    fill_in 'Last Name', with: 'Sampson'
+    fill_in 'Username', with: 'Swansonite11'
+    fill_in 'Email', with: 'jeffsampson@example.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    click_button 'Sign up'
 
-      click_button 'Sign up'
-    end
-    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content('You\'re in! Welcome to Lodestar')
     expect(page).to have_content('Sign Out')
   end
 
   scenario 'provide invalid registration information' do
-    visit new_user_registration_path
-    within('#new-user') do
-      click_button 'Sign up'
-    end
-    expect(page).to have_content("First name can't be blank")
-    expect(page).to have_content("Last name can't be blank")
-    expect(page).to have_content("Username name can't be blank")
-    expect(page).to have_content("Email can't be blank")
-    expect(page).to have_content("Password can't be blank")
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign up'
+    expect(page).to have_content('can\'t be blank')
+    expect(page).to_not have_content('Sign Out')
+  end
+
+  scenario 'password confirmation does not match confirmation' do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'user_password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password2'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content('doesn\'t match')
     expect(page).to_not have_content('Sign Out')
   end
 end
