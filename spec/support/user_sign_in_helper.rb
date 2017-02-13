4e28bbe83a5bc37f14ed893485e205e0044046de
Erik Warringer <erikwarringer@gmail.com>
2017-02-12 22:20:08 -0500

    changes

--- spec/support/user_sign_in_helper.rb
+++ spec/support/user_sign_in_helper.rb
@@ -4,6 +4,6 @@ module UserSignIn
     click_link "Sign_in"
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_button 'Log in'
+    click_button 'Sign in'
   end
 end
